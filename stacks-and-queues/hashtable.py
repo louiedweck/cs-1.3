704ad074cb6d7b494954afd5436f350cd5e5cb89
louiedweck <dweck.louis@gmail.com>
2020-05-14 01:41:32 -0400

    ques, hashtables cleaned up. Tree methods 1/2 done.

--- stacks-and-queues/hashtable.py
+++ stacks-and-queues/hashtable.py
@@ -6,7 +6,7 @@ class HashTable(object):
     def __init__(self, init_size=8):
         """Initialize this hash table with the given initial size."""
         self.buckets = [LinkedList() for i in range(init_size)]
-        self.size = 0  # Number of key-value entries
+        self.size = init_size  # Number of key-value entries
 
     def __str__(self):
         """Return a formatted string representation of this hash table."""
@@ -32,7 +32,7 @@ class HashTable(object):
         # Collect all keys in each of the buckets
         all_keys = []
         for bucket in self.buckets:
-            for key, value in bucket.items():
+            for key, _ in bucket.items():
                 all_keys.append(key)
         return all_keys
 
@@ -42,7 +42,7 @@ class HashTable(object):
         # Collect all values in each of the buckets
         all_values = []
         for bucket in self.buckets:
-            for key, value in bucket.items():
+            for _, value in bucket.items():
                 all_values.append(value)
         return all_values
 
--- stacks-and-queues/hashtable_test.py
+++ stacks-and-queues/hashtable_test.py
@@ -2,8 +2,8 @@ from hashtable import HashTable
 import unittest
 
 # Python 2 and 3 compatibility: unittest module renamed this assertion method
-if not hasattr(unittest.TestCase, 'assertCountEqual'):
-    unittest.TestCase.assertCountEqual = unittest.TestCase.assertItemsEqual
+# if not hasattr(unittest.TestCase, 'assertCountEqual'):
+#     unittest.TestCase.assertCountEqual = unittest.TestCase.assertItemsEqual
 
 
 class HashTableTest(unittest.TestCase):
